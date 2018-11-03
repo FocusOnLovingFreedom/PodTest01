a83dbc6ad20f66dd4b32ddbc79d10dcddc82e2c2
lishan <lishan@lishandeMacBook-Pro.local>
2018-11-03 21:58:34 +0800

    初始化工程

---
+++ TestPodViewTouch/TestPodViewTouchTests/TestPodViewTouchTests.m
@@ -0,0 +1,39 @@
+//
+//  TestPodViewTouchTests.m
+//  TestPodViewTouchTests
+//
+//  Created by lishan on 2018/11/3.
+//  Copyright © 2018年 lishan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestPodViewTouchTests : XCTestCase
+
+@end
+
+@implementation TestPodViewTouchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
